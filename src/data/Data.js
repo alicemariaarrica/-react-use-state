0f268859f7397daf5e6aa025815cfc79c24f67e8
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-17 20:43:03 +0100

    data-js-array-commit

---
+++ src/data/Data.js
@@ -0,0 +1,8 @@
+
+export const languages = [
+    { name: "HTML", description: "HTML è il linguaggio di markup per la struttura delle pagine web." },
+    { name: "CSS", description: "CSS è utilizzato per stilizzare il layout e il design delle pagine web." },
+    { name: "JavaScript", description: "JavaScript è un linguaggio di programmazione che consente di creare interazioni dinamiche nelle pagine web." },
+    { name: "PHP", description: "PHP è un linguaggio di scripting lato server, utilizzato principalmente per lo sviluppo di applicazioni web." },
+    { name: "Python", description: "Python è un linguaggio di programmazione versatile, usato anche per lo sviluppo web tramite framework come Django." }
+];
