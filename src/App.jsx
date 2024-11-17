687f89819ebda6ac615ae49688f84b6f6dc0fe82
alicemariaarrica <alicemariaarrica@gmail.com>
2024-11-17 21:32:07 +0100

    app-jsx-commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,4 +4,32 @@
 //Inserisci sotto una card che mostri il titolo e la descrizione del primo linguaggio nell’array.
 //Fare in modo che, cliccando uno dei bottoni, la card in basso visualizzi il linguaggio corrispondente e la relativa descrizione
 
+import React from 'react';
+import { createFunction } from './hooks/createFunction';
+import './App.css';
 
+function App() {
+
+  const { selectedCard, selectCard } = createFunction();
+
+  return (
+    <><div className="App">
+
+      <div className="buttons-container">
+        {data.map((card, index) => (
+          <button
+            key={index}
+            onClick={() => selectCard(card)}
+
+            {...card.name} />))}
+      </button>
+      ))}
+    </div><div className="card">
+        <h2>{selectedCard.name}</h2>
+        <p>{selectedCard.description}</p>
+      </div></>
+    </div >
+  );
+}
+
+export default App;
